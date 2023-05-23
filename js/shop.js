cee5ff139acf4a39b1311d60b890ee448aad3e30
REALcuba <angelskcuba@gmail.com>
2023-05-23 21:59:53 +0200

    buy,calculate total y clear cart done 70%, missing Qty y Qty-totalPrice

--- js/shop.js
+++ js/shop.js
@@ -1,143 +1,167 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
 var products = [
-   {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
-    },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
+  {
+    id: 1,
+    name: 'cooking oil',
+    price: 10.5,
+    type: 'grocery',
+    offer: {
+      number: 3,
+      percent: 20
     }
+  },
+  {
+    id: 2,
+    name: 'Pasta',
+    price: 6.25,
+    type: 'grocery'
+  },
+  {
+    id: 3,
+    name: 'Instant cupcake mixture',
+    price: 5,
+    type: 'grocery',
+    offer: {
+      number: 10,
+      percent: 30
+    }
+  },
+  {
+    id: 4,
+    name: 'All-in-one',
+    price: 260,
+    type: 'beauty'
+  },
+  {
+    id: 5,
+    name: 'Zero Make-up Kit',
+    price: 20.5,
+    type: 'beauty'
+  },
+  {
+    id: 6,
+    name: 'Lip Tints',
+    price: 12.75,
+    type: 'beauty'
+  },
+  {
+    id: 7,
+    name: 'Lawn Dress',
+    price: 15,
+    type: 'clothes'
+  },
+  {
+    id: 8,
+    name: 'Lawn-Chiffon Combo',
+    price: 19.99,
+    type: 'clothes'
+  },
+  {
+    id: 9,
+    name: 'Toddler Frock',
+    price: 9.99,
+    type: 'clothes'
+  }
 ]
 // import { products } from './products.js';
-// const{id,name,price,type,offer}=products;
-
 // Array with products (objects) added directly with push(). Products in this array are repeated.
-var cartList = [];
+var cartList = []
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
-var cart = [];
+var cart = []
 
-var total = 0;
+var total = 0
+
+// getElements
+const cartArr = document.getElementById('cart_list')
+const totalPrice = document.getElementById('total_price')
 
 // Exercise 1
- function buy(id) {
+function buy (id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
-    for(let i =0; i<products.length; i++){
-        if(products[i].id == id){
-            cartList.push(products[i]);
-        }
-      console.log('cartList',cartList);
+  const cartArr = document.getElementById('cart_list')
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === id) {
+      cartList.push(products[i])
     }
-    //refactor with find() method
-    //const product = products.find(product => product.id === id);
-    //if (product) {
+    // console.log('cartList', cartList)
+  }
+  cartArr.innerHTML = cartList.map(product => `<tr>
+  <th scope="row">${product.name}</th>
+    <td>${product.price}</td>
+    <td>${product.id}</td>
+    <td>${product.type}</td>
+  </tr>`)
+  calculateTotal()
+            // )
+    // refactor with find() method
+    // const product = products.find(product => product.id === id);
+    // if (product) {
     //   cartList.push(product);
     //  }
 }
 
 // Exercise 2
-function cleanCart() {
-if(cartList.length > 0){
-    cartList = [];
-     console.log("Cart cleaned");
-     console.log(cartList);
-}
+function cleanCart () {
+  if (cartList.length > 0) {
+    cartList = []
+    console.log('Cart cleaned')
+    cartArr.innerHTML = []
+    totalPrice.innerText = ''
+  }
 }
 
 // Exercise 3
-function calculateTotal() {
+function calculateTotal () {
     // Calculate total price of the cart using the "cartList" array
+
+  if (cartList.length > 0) {
+    for (let i = 0; i < cartList.length; i++) {
+      total += cartList[i].price
+    }
+    console.log('Total: $' + `${total}`)
+    totalPrice.innerText = `${total}`
+  }
 }
 
 // Exercise 4
-function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
+function generateCart () {
+    // Using the "cartlist" array that contains all the items in the shopping cart,
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
 }
 
 // Exercise 5
-function applyPromotionsCart() {
+function applyPromotionsCart () {
     // Apply promotions to each item in the array "cart"
 }
 
 // Exercise 6
-function printCart() {
+function printCart () {
     // Fill the shopping cart modal manipulating the shopping cart dom
 }
-
 
 // ** Nivell II **
 
 // Exercise 7
-function addToCart(id) {
-    // Refactor previous code in order to simplify it 
+function addToCart (id) {
+    // Refactor previous code in order to simplify it
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
 }
 
 // Exercise 8
-function removeFromCart(id) {
+function removeFromCart (id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
 }
 
-function open_modal(){
-	console.log("Open Modal");
-	printCart();
+function open_modal () {
+  console.log('Open Modal')
+  printCart()
 }
+
+calculateTotal()
